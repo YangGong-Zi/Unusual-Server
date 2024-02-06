cacd9b15755befd073c535c4d4ec128825c39aca
wangzewei <dream_run@126.com>
2024-02-06 15:27:28 +0800

    🐞 fix(配置文件不已.开始)

--- src/utils/yml.config.ts
+++ src/utils/yml.config.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 const env = process.env.NODE_ENV
 export const getYmlConfig = (key?: string) => {
     const ymlInfo = yaml.load(
-        readFileSync(join(process.cwd(), `.config/.${env}.yml`), 'utf-8'),
+        readFileSync(join(process.cwd(), `config/${env}.yml`), 'utf-8'),
     ) as Record<string, any>;
     if (key) {
         return ymlInfo[key];
